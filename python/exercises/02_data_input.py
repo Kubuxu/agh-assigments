9f71e060bd2e10502c3d1434c48301a34a434330
Jakub Sztandera <kubuxu@protocol.ai>
2020-12-13 17:15:21 +0100

    Add first two tasks
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

---
+++ python/exercises/02_data_input.py
@@ -0,0 +1,10 @@
+print("Enter your First Name:")
+fname = input()
+
+print("Enter your Last Name:")
+lname = input()
+
+print("Enter your birthdate:")
+bdate = input()
+
+print(f"Hi {fname} {lname}. You were born {bdate}.")
